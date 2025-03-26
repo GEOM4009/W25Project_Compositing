5ff8f9f9a47ba0b065f2b38112e8e8822b3d9f27
adricaswell <adrianacaswell@gmail.com>
2025-03-26 15:58:24 -0400

    setup sphinx auto documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup -------------------------------------------------------
+# If extensions (or modules to document with autodoc) are in another d
+# add these directories to sys.path here. If the directory is relative
+# documentation root, use os.path.abspath to make it absolute, like sh
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Sentinel-2 Compositing Tool"
+copyright = "2025, Adriana, Christian, and Muhammad"
+author = "Adriana, Christian, and Muhammad"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "numpydoc",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/source/modules.rst
@@ -0,0 +1,7 @@
+W25Project_Compositing
+======================
+
+.. toctree::
+   :maxdepth: 4
+
+   s2composite
