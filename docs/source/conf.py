f2ac51124b71c8ecb5af6614a83f2dae06c61d60
adricaswell <adrianacaswell@gmail.com>
2025-04-08 15:10:17 -0400

    update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 project = "Sentinel-2 Compositing Tool"
 copyright = "2025, Adriana Caswell, Christian Devey, and Muhammad Ba"
 author = "Adriana Caswell, Christian Devey, and Muhammad Ba"
-release = "0.1"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
